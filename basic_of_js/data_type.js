37f9ed8b08613f216d49bd40ae6e30d6908c5385
Suryam Gahoi <suryamgahoi14@gmail.com>
2025-10-07 17:39:37 +0000

    learn about dataType in JS

---
+++ basic_of_js/data_type.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(2+3);  this will only run in browser we cann't perform by node compiler
+
+let name = "Suryam Gahoi" // string data type
+let age = 18 // integer data type
+let isOffer = true // boolean data type
+let price = 2999999.99 // number data type
+let temperature = null // null not any value 
+let state; // undefined 
+
+
+/* 
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standAlone value
+undefined => value isn't defined
+symbol => unique
+*/
+
+// object 
+
+
+console.table([typeof name, typeof age, typeof isOffer, typeof price, typeof temperature, typeof state])
+
+
+/* output
+console.log(typeof undefined) => undefined
+console.log(typeof null) => object
+*/
