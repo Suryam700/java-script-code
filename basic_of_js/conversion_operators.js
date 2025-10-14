cba127ddb7b9f1abb57ee9a17416cf0cf92cf436
Suryam Gahoi <suryamgahoi14@gmail.com>
2025-10-14 18:22:45 +0000

    add some operation's

--- basic_of_js/conversion_operators.js
+++ basic_of_js/conversion_operators.js
@@ -62,3 +62,40 @@ console.log(typeof(NaN)) // number
         undefined => string...
     */
 }
+
+
+// *************************OPERATORS****************************
+let value = 4
+let negValue = -value
+console.log(negValue)
+
+console.log(3+2)   // 5
+console.log(3-2)   // 1
+console.log(3*2)   // 6
+console.log(3/2)   // 1.5
+console.log(3**2)  // 9
+console.log(3%2)   // 1
+
+let str1 = "Hello, "
+let str2 = "Suryam Gahoi /\\"
+let newStr = str1 + str2
+console.log(str1 + str2)
+
+console.log("1"+ 2)
+console.log(typeof(1+ "2"))
+console.log("1"+ 2+ 2)
+console.log(1+ 1+ "2")
+
+console.log(+true)     // 1
+console.log(+"")       // 0
+console.log(true + 1)  // 2
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++ // 101
+++gameCounter    // 101
+console.log(gameCounter)
+
+// for more we can explore on MDN web docs
